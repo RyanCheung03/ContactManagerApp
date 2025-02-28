1a01aacfcb5c1abedec29e880303f206ecf0c2f8
RyanCheung03 <ryanch246@gmail.com>
2025-02-27 21:33:32 -0500

    Initial commit

---
+++ ContactManagerApp/Models/Contact.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ContactManagerApp.Models
+{
+	public class Contact
+	{
+        public int Id { get; set; }
+        
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+        
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+        
+        [Required(ErrorMessage = "Phone number is required")]
+        [Phone(ErrorMessage = "Invalid Phone Number")]
+        public string Phone { get; set; }
+        
+        public string Company { get; set; }
+    }
+}
---
+++ ContactManagerApp/Models/ContactContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace ContactManagerApp.Models
+{
+    public class ContactContext : DbContext
+    {
+        public ContactContext() : base("name=DefaultConnection")
+        {
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
